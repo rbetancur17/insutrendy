ca8d4805c87a7cf19760e502c470af354005536e
rbetancur17 <53304452+rbetancur17@users.noreply.github.com>
2019-08-14 06:32:02 -0500

    confirm

--- webapp/resources/views/welcome.blade.php
+++ webapp/resources/views/welcome.blade.php
@@ -8,7 +8,13 @@
         {{-- aca iria el foreach --}}
         <div class="row">
             <div class="col-12 col-sm-12 col-md-4 col-xl-4">
-                <img src="{{ asset('img/product/3.jpg') }}" alt="Insutrendy" class="img-fluid img-thumbail w-75">
+                <img src="{{ asset('img/product/boton2.jpg') }}" alt="Insutrendy" class="img-fluid featured-products">
+            </div>
+            <div class="col-12 col-sm-12 col-md-4 col-xl-4">
+                <img src="{{ asset('img/product/boton1.jpg') }}" alt="Insutrendy" class="img-fluid featured-products">
+            </div>
+            <div class="col-12 col-sm-12 col-md-4 col-xl-4">
+                <img src="{{ asset('img/product/boton1.jpg') }}" alt="Insutrendy" class="img-fluid featured-products">
             </div>
         </div>
     </div>
