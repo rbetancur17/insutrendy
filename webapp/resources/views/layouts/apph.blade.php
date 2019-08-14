4caf4dc2b22eb015c5e88285b9351ca5d5037727
rbetancur17 <53304452+rbetancur17@users.noreply.github.com>
2019-08-14 10:41:30 -0500

    confirm

--- webapp/resources/views/layouts/apph.blade.php
+++ webapp/resources/views/layouts/apph.blade.php
@@ -194,7 +194,7 @@
     <section class="footer-section">
 		<div class="container">
 			<div class="footer-logo text-center">
-				<a href="index.html"><img src="./img/logo-light.png" alt=""></a>
+				<a href="index.html"><img src="{{ asset('img/logo_trans.png') }}" alt=""></a>
 			</div>
 			<div class="row">
 				<div class="col-lg-3 col-sm-6">
--- webapp/resources/views/welcome.blade.php
+++ webapp/resources/views/welcome.blade.php
@@ -4,17 +4,24 @@
 @section('content')
     <br>
     <h2 class="text-center">Productos Destacados</h2>
+    <hr class="w-75">
     <div class="container">
         {{-- aca iria el foreach --}}
         <div class="row">
             <div class="col-12 col-sm-12 col-md-4 col-xl-4">
-                <img src="{{ asset('img/product/boton2.jpg') }}" alt="Insutrendy" class="img-fluid featured-products">
+                <img src="{{ asset('img/product/boton2.jpg') }}" alt="Insutrendy" class="featured-products">
+                <span><p>Boton</p></span>
+                <button class="btn btn-success">Solicitar Información</button>
             </div>
             <div class="col-12 col-sm-12 col-md-4 col-xl-4">
-                <img src="{{ asset('img/product/boton1.jpg') }}" alt="Insutrendy" class="img-fluid featured-products">
+                <img src="{{ asset('img/product/boton1.jpg') }}" alt="Insutrendy" class="featured-products">
+                <span><p>Boton</p></span>
+                <button class="btn btn-success">Solicitar Información</button>
             </div>
             <div class="col-12 col-sm-12 col-md-4 col-xl-4">
-                <img src="{{ asset('img/product/boton1.jpg') }}" alt="Insutrendy" class="img-fluid featured-products">
+                <img src="{{ asset('img/product/boton3.jpg') }}" alt="Insutrendy" class="featured-products">
+                <span><p>Boton</p></span>
+                <button class="btn btn-success">Solicitar Información</button>
             </div>
         </div>
     </div>
