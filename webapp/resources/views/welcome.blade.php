95800720128f40d5d31759c09bb8e341016b8a2c
rbetancur17 <53304452+rbetancur17@users.noreply.github.com>
2019-08-13 22:06:30 -0500

    confirm

--- webapp/resources/views/welcome.blade.php
+++ webapp/resources/views/welcome.blade.php
@@ -2,5 +2,17 @@
 
 @extends('layouts.apph')
 @section('content')
-    
+    <br>
+    <div class="container">
+        {{-- aca iria el foreach --}}
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-4 col-xl-4">
+                <img src="{{ asset('img/product/3.jpg') }}" alt="Insutrendy" class="img-fluid img-thumbail w-75">
+            </div>
+        </div>
+    </div>
+    <br>
+    <hr>
+
+
 @endsection
