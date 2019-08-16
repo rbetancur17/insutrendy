57c3477dc12870a593c4f225c539b912733c84da
Steven Betancur <steven.betancur@sured.com.co>
2019-08-16 01:29:29 -0500

    modificacion de menu

--- webapp/bd/script.sql
+++ webapp/bd/script.sql
@@ -25,20 +25,17 @@ CREATE TABLE IF NOT EXISTS `categories` (
   `created_at` date DEFAULT NULL,
   `updated_at` date DEFAULT NULL,
   `deleted_at` date DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_categorie`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla insutrendy.categories: ~6 rows (aproximadamente)
+-- Volcando datos para la tabla insutrendy.categories: ~3 rows (aproximadamente)
 DELETE FROM `categories`;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` (`id_categorie`, `code`, `description`, `name`, `created_at`, `updated_at`, `deleted_at`) VALUES
-	(1, '002', 'sdsdfsdf', '123132', '2019-08-10', '2019-08-10', NULL),
-	(2, '002', 'sdsdfsdf', '123132', '2019-08-10', '2019-08-10', NULL),
-	(3, '002', 'casasdasdasdasdasdasdadadasdasdasdasd', 'Categoria 1', '2019-08-10', '2019-08-10', NULL),
-	(4, '003', 'scasdasdasdasdasdasdasdasd', 'Categoria 2', '2019-08-10', '2019-08-10', NULL),
-	(5, '003', 'scasdasdasdasdasdasdasdasd', 'Categoria 2', '2019-08-10', '2019-08-10', NULL),
-	(6, '004', 'asdasdasdasd', 'Categoria 1', '2019-08-10', '2019-08-10', NULL),
-	(7, '004', '1000000', '1000', '2019-08-10', '2019-08-10', NULL);
+INSERT INTO `categories` (`id_categorie`, `code`, `description`, `name`, `created_at`, `updated_at`, `deleted_at`, `status`) VALUES
+	(8, '001', 'Zapatos y Bolsos', 'Zapatos y Bolsos', '2019-08-16', '2019-08-16', NULL, NULL),
+	(9, '003', 'Vestuario', 'Vestuario', '2019-08-16', '2019-08-16', NULL, NULL),
+	(10, '002', 'Accesorios', 'Accesorios', '2019-08-16', '2019-08-16', NULL, NULL);
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 
 -- Volcando estructura para tabla insutrendy.products
@@ -48,19 +45,62 @@ CREATE TABLE IF NOT EXISTS `products` (
   `name` varchar(200) DEFAULT NULL,
   `description` varchar(200) DEFAULT NULL,
   `price` int(11) DEFAULT NULL,
-  `id_categorie` int(11) DEFAULT NULL,
   `created_at` date DEFAULT NULL,
+  `id_subcategorie` int(11) DEFAULT NULL,
   `updated_at` date DEFAULT NULL,
   `deleted_at` date DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `outstanding` varchar(50) DEFAULT NULL,
+  `image` varchar(500) DEFAULT NULL,
+  `image2` varchar(500) DEFAULT NULL,
+  `image3` varchar(500) DEFAULT NULL,
+  `image4` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id_product`),
-  KEY `FK_products_categories` (`id_categorie`),
-  CONSTRAINT `FK_products_categories` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id_categorie`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `FK_products_subcategories` (`id_subcategorie`),
+  CONSTRAINT `FK_products_subcategories` FOREIGN KEY (`id_subcategorie`) REFERENCES `subcategories` (`id_subcategorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 
 -- Volcando datos para la tabla insutrendy.products: ~0 rows (aproximadamente)
 DELETE FROM `products`;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
+INSERT INTO `products` (`id_product`, `code`, `name`, `description`, `price`, `created_at`, `id_subcategorie`, `updated_at`, `deleted_at`, `status`, `outstanding`, `image`, `image2`, `image3`, `image4`) VALUES
+	(3, '1', 'Suela de zapato', 'Suela de producto', 10000, '2019-08-16', 1, '2019-08-16', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(4, '02', 'Suela de zapato', 'suela de zapto', 5000, '2019-08-16', 1, '2019-08-16', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(5, '3', 'Herraje de collar', 'Herraje de collar', 1000, '2019-08-16', 2, '2019-08-16', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(9, '5', 'Camiseta china', 'Camiseta china', NULL, '2019-08-16', 4, '2019-08-16', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(10, '6', 'Jeans chinos', 'Jeans chinos', 5000, '2019-08-16', 5, '2019-08-16', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(11, '7', 'Vestido chino largo', 'Vestido chino largo', NULL, '2019-08-16', 6, '2019-08-16', NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+	(12, '9', 'Vestido rojo chino', 'Vestido rojo chino', 1000, '2019-08-16', 6, '2019-08-16', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
+
+-- Volcando estructura para tabla insutrendy.subcategories
+CREATE TABLE IF NOT EXISTS `subcategories` (
+  `id_subcategorie` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '0',
+  `description` varchar(100) NOT NULL DEFAULT '0',
+  `id_categorie` int(11) NOT NULL DEFAULT '0',
+  `code` varchar(50) DEFAULT NULL,
+  `created_at` date DEFAULT NULL,
+  `updated_at` date DEFAULT NULL,
+  `deleted_at` date DEFAULT NULL,
+  PRIMARY KEY (`id_subcategorie`),
+  KEY `FK_subcategories_categories` (`id_categorie`),
+  CONSTRAINT `FK_subcategories_categories` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id_categorie`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+-- Volcando datos para la tabla insutrendy.subcategories: ~8 rows (aproximadamente)
+DELETE FROM `subcategories`;
+/*!40000 ALTER TABLE `subcategories` DISABLE KEYS */;
+INSERT INTO `subcategories` (`id_subcategorie`, `name`, `description`, `id_categorie`, `code`, `created_at`, `updated_at`, `deleted_at`) VALUES
+	(1, 'Suelas', 'Suelas', 8, '001', '2019-08-16', '2019-08-16', NULL),
+	(2, 'Herrajes', 'Herrajes', 8, '002', '2019-08-16', '2019-08-16', NULL),
+	(3, 'Producto Terminado', 'Producto Terminado', 8, '003', '2019-08-16', '2019-08-16', NULL),
+	(4, 'Camiseta', 'Camiseta', 9, '005', '2019-08-16', '2019-08-16', NULL),
+	(5, 'Jeans', 'Jeans', 9, '006', '2019-08-16', '2019-08-16', NULL),
+	(6, 'Vestido', 'Vestido', 9, '007', '2019-08-16', '2019-08-16', NULL),
+	(7, 'Collares', 'collares', 10, '009', '2019-08-16', '2019-08-16', NULL),
+	(8, 'Aretes', 'Aretes', 10, '010', '2019-08-16', '2019-08-16', NULL);
+/*!40000 ALTER TABLE `subcategories` ENABLE KEYS */;
 
 -- Volcando estructura para tabla insutrendy.users
 CREATE TABLE IF NOT EXISTS `users` (
