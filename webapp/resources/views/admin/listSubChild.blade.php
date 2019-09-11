d2c4354cd30c5c292e009da7c913687d903a8188
rbetancur17 <53304452+rbetancur17@users.noreply.github.com>
2019-09-10 20:36:54 -0500

    confirm

--- webapp/resources/views/admin/listSubChild.blade.php
+++ webapp/resources/views/admin/listSubChild.blade.php
@@ -9,7 +9,7 @@
             <div class="col-12 col-sm-12 col-md-12 col-lg-12">
                 <div class="card">
                 <div class="card-header card-header-primary">
-                  <h4 class="card-title ">Listado de SubCategorias</h4>
+                  <h4 class="card-title ">Listado de SubCategorias Hijas</h4>
                   <p class="card-category"></p>
                 </div>
                 <div class="card-body">
