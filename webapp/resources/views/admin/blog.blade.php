6a78ff6a9e56935d0eb2dc6b7351cc022c1e6a3e
rbetancur17 <53304452+rbetancur17@users.noreply.github.com>
2019-09-24 20:42:53 -0500

    confirm

--- webapp/resources/views/admin/blog.blade.php
+++ webapp/resources/views/admin/blog.blade.php
@@ -97,7 +97,48 @@
             "insertdatetime media nonbreaking save table contextmenu directionality",
             "emoticons template paste textcolor colorpicker textpattern"
           ],
-          toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
+          toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media | forecolor backcolor",
+          color_map: [
+            "000000", "Black",
+            "993300", "Burnt orange",
+            "333300", "Dark olive",
+            "003300", "Dark green",
+            "003366", "Dark azure",
+            "000080", "Navy Blue",
+            "333399", "Indigo",
+            "333333", "Very dark gray",
+            "800000", "Maroon",
+            "FF6600", "Orange",
+            "808000", "Olive",
+            "008000", "Green",
+            "008080", "Teal",
+            "0000FF", "Blue",
+            "666699", "Grayish blue",
+            "808080", "Gray",
+            "FF0000", "Red",
+            "FF9900", "Amber",
+            "99CC00", "Yellow green",
+            "339966", "Sea green",
+            "33CCCC", "Turquoise",
+            "3366FF", "Royal blue",
+            "800080", "Purple",
+            "999999", "Medium gray",
+            "FF00FF", "Magenta",
+            "FFCC00", "Gold",
+            "FFFF00", "Yellow",
+            "00FF00", "Lime",
+            "00FFFF", "Aqua",
+            "00CCFF", "Sky blue",
+            "993366", "Red violet",
+            "FFFFFF", "White",
+            "FF99CC", "Pink",
+            "FFCC99", "Peach",
+            "FFFF99", "Light yellow",
+            "CCFFCC", "Pale green",
+            "CCFFFF", "Pale cyan",
+            "99CCFF", "Light sky blue",
+            "CC99FF", "Plum"
+            ],
           relative_urls: false,
           file_browser_callback : function(field_name, url, type, win) {
             var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
@@ -109,7 +150,9 @@
             } else {
               cmsURL = cmsURL + "&type=Files";
             }
-      
+            
+            
+
             tinyMCE.activeEditor.windowManager.open({
               file : cmsURL,
               title : 'Filemanager',
